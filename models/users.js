0f459913d4726b4b780817286d12e60e63c6acf6
Ujjwal Bhusal <45995959+bhusalujjwal@users.noreply.github.com>
2020-01-23 14:42:31 +0545

    added user models

--- models/users.js
+++ models/users.js
@@ -1,1 +1,35 @@
-//users list details
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: 6
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String
+    },
+    admin: {
+        type: Boolean,
+        default: false
+    }
+});
+
+module.exports = mongoose.model('User', userSchema);
