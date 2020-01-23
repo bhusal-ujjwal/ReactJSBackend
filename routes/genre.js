fb10354b551fbac4825d937859018dd87ac27fb5
Ujjwal Bhusal <45995959+bhusalujjwal@users.noreply.github.com>
2020-01-23 14:34:47 +0545

    added models and routes

---
+++ routes/genre.js
@@ -0,0 +1,1 @@
+// genre type for novel 
---
+++ routes/users.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/users');
+const router = express.Router();
+const auth = require('../auth');
+
+router.post('/signup', (req, res, next) => {
+    let password = req.body.password;
+    bcrypt.hash(password, 10, function (err, hash) {
+        if (err) {
+            let err =  new Error('Could not hash!');
+		err.status = 500;
+		return next(err);
+        }
+        User.create({
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.body.email,
+            username: req.body.username,
+            password: hash,
+            image: req.body.image
+        }).then((user) => {
+            let token = jwt.sign({ _id: user._id }, process.env.SECRET);
+            res.json({ status: "Signup done successfully!", token: token });
+        }).catch(next);
+    });
+});
+
+router.post('/login', (req, res, next) => {
+    User.findOne({ username: req.body.username })
+        .then((user) => {
+            if (user == null) {
+                let err = new Error('User not found!');
+                err.status = 401;
+                return next(err);
+            } else {
+                bcrypt.compare(req.body.password, user.password)
+                    .then((isMatch) => {
+                        if (!isMatch) {
+                            let err = new Error('Password does not match!');
+                            err.status = 401;
+                            return next(err);
+                        }
+                        let token = jwt.sign({ _id: user._id }, process.env.SECRET);
+                        res.json({ status: 'Login successfully!', token: token });
+                    }).catch(next);
+            }
+        }).catch(next);
+})
+
+router.get('/me', auth.verifyUser, (req, res, next) => {
+    res.json({ _id: req.user._id, firstName: req.user.firstName, lastName: req.user.lastName, email: req.user.email, username: req.user.username, image: req.user.image });
+});
+
+router.put('/me', auth.verifyUser, (req, res, next) => {
+    User.findByIdAndUpdate(req.user._id, { $set: req.body }, { new: true })
+        .then((user) => {
+            res.json({ _id: user._id, firstName: req.user.firstName, lastName: req.user.lastName, email: req.user.email,username: user.username, image: user.image });
+        }).catch(next);
+});
+
+module.exports = router;
