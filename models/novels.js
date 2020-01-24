dc9bb28ed103971a8484e7aca616d60e902110d6
Ujjwal Bhusal <45995959+bhusalujjwal@users.noreply.github.com>
2020-01-24 11:50:30 +0545

    some things

--- models/novels.js
+++ models/novels.js
@@ -1,1 +1,44 @@
-//novel details 
+const mongoose = require('mongoose');
+const chaptercontentSchema = new mongoose.Schema({
+    note: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true });
+const novelSchema = new mongoose.Schema({
+    name :
+    {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String
+    },
+    desc:
+    {
+        type: String,
+        required: true
+    },
+    author:
+    {
+        type: String,
+        required: true
+    },
+    genre:
+    {
+        type: String,
+        required: true
+    },
+    chapters:
+    {
+        type: Number,
+        required: true
+    },
+    genre:
+    {
+        type: String,
+        required: true
+    },
+    chaptercontent: [chaptercontentSchema],
+},{timestamps:true});
+module.exports = mongoose.model('Novel', novelSchema);
