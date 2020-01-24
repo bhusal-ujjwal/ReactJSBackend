584fc1263cdb84383ce27b73d7b80cdaf8f81cc8
Ujjwal Bhusal <45995959+bhusalujjwal@users.noreply.github.com>
2020-01-24 10:38:44 +0545

    added genre schema

--- models/genre.js
+++ models/genre.js
@@ -1,1 +1,11 @@
-// genre type for novel 
+const mongoose = require('mongoose');
+
+const genreSchema= new mongoose.Schema({
+    genre: {
+        type:String,
+        required:true
+    }
+
+},{timestamps:true});
+
+module.exports=mongoose.model('Genre',genreSchema);
